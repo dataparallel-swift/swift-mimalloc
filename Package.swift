f90699e9e7c752efb4b7c055dc063f49f9b7844a
Trevor L. McDonell <trevor@passivelogic.com>
2025-10-03 11:20:41 +0200

    build: add package traits for cuda and/or malloc interposition

--- Package.swift
+++ Package.swift
@@ -17,6 +17,15 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(name: "swift-mimalloc", targets: ["swift-mimalloc"]),
     ],
+    traits: [
+        // Abusing the traits system, since these are (currently) mutually
+        // exclusive. Add a default trait because we can't make build conditions
+        // that are negations (i.e. when the CUDA trait is not enabled), or for
+        // when no traits are enabled.
+        "CUDA",
+        "Interpose",
+        .default(enabledTraits: ["Interpose"]),
+    ],
     dependencies: [
         .package(url: "git@gitlab.com:PassiveLogic/compiler/swift-cuda.git", from: "0.2.0"),
     ],
@@ -26,23 +35,27 @@ let package = Package(
         .target(
             name: "swift-mimalloc",
             dependencies: [
-                .product(name: "CUDA", package: "swift-cuda"),
+                .product(name: "CUDA", package: "swift-cuda", condition: .when(traits: ["CUDA"])),
             ],
             path: "Sources/mimalloc-cbits",
             sources: ["mimalloc-cbits.c"],
             publicHeadersPath: ".",
             cSettings: [
                 .headerSearchPath("mimalloc/include"),
-                .define("MI_USE_CUDA"),
                 .define("NDEBUG", .when(configuration: .release)),
                 .define("MI_BUILD_RELEASE", .when(configuration: .release)),
                 .define("MI_OPT_SIMD", to: "1", .when(configuration: .release)),
-                // .define("MI_MALLOC_OVERRIDE"),
-                // .define("MI_OSX_ZONE", to: "1", .when(platforms: [.macOS])),
-                // .define("MI_OSX_INTERPOSE", to: "1", .when(platforms: [.macOS])),
-                // .define("MI_WIN_NOREDIRECT", to: "1", .when(platforms: [.windows])),
-                // .unsafeFlags(["-fno-builtin-malloc"]),
                 .unsafeFlags(archFlags, .when(configuration: .release)),
+
+                // Use CUDA as the backing allocator
+                .define("MI_USE_CUDA", .when(traits: ["CUDA"])),
+
+                // Enable malloc interposition (currently not compatible with CUDA backend)
+                .define("MI_MALLOC_OVERRIDE", .when(traits: ["Interpose"])),
+                .define("MI_OSX_ZONE", to: "1", .when(platforms: [.macOS], traits: ["Interpose"])),
+                .define("MI_OSX_INTERPOSE", to: "1", .when(platforms: [.macOS], traits: ["Interpose"])),
+                .define("MI_WIN_NOREDIRECT", to: "1", .when(platforms: [.windows], traits: ["Interpose"])),
+                .unsafeFlags(["-fno-builtin-malloc"], .when(traits: ["Interpose"])),
             ],
         ),
     ],
