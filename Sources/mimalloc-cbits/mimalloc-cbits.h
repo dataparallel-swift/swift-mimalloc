c345a8878f888318ce2c4a8adc67bbc6c5ebe4a5
Trevor L. McDonell <trevor@passivelogic.com>
2025-12-05 15:40:58 +0100

    docs: update file headers

--- Sources/mimalloc-cbits/mimalloc-cbits.h
+++ Sources/mimalloc-cbits/mimalloc-cbits.h
@@ -1,7 +1,19 @@
-/*
- * mi-malloc: a compact general-purpose allocator with excellent performance
- * https://microsoft.github.io/mimalloc
- */
+// Copyright (c) 2025 The swift-mimalloc authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// mi-malloc: a compact general-purpose allocator with excellent performance
+// https://microsoft.github.io/mimalloc
 
 #ifndef __MIMALLOC_CBITS_H__
 #define __MIMALLOC_CBITS_H__
